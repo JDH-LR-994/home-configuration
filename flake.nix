54a79db04c57256f4ff3a440d9f30f4920bdf209
JDH-LR-994 <jdh994lr@gmail.com>
2025-09-29 22:35:10 +0300

    Use nixvim

--- flake.nix
+++ flake.nix
@@ -8,10 +8,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+		nixvim = {
+			url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+		};
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    { nixpkgs, home-manager, nixvim, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -20,12 +24,11 @@
       homeConfigurations."andrey" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+        modules = [ 
+				  ./home.nix
+					nixvim.homeModules.nixvim
+				];
+      
+			};
+  };
 }
--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,20 +1,59 @@
 { pkgs, ... }:
 
 {
-  programs.neovim = {
+
+  programs.nixvim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
 
-    plugins = with pkgs.vimPlugins; [
-      catppuccin-nvim
-    ];
+    plugins = with pkgs.vimPlugins; {
+      
+    };
+
+		colorschemes.catppuccin = {
+			enable = true;
+			settings = {
+				flavour = "mocha";
+			};
+		};
 
     extraPackages = with pkgs; [
       tree-sitter
     ];
+
+		opts = {
+			number = true;
+			relativenumber = true;
+			tabstop = 2;
+			shiftwidth = 2;
+			smartindent = true;
+			termguicolors = true;
+		};
+
+		globals = {
+			mapleader = " ";
+		};
+
+		keymaps = [
+		  {
+			  key = "<leader>w";
+				action = ":w<CR>";
+				options.desc = "Save file";
+			}
+			{
+				key = "<leader>q";
+				action = ":q<CR>";
+				options.desc = "Quit";
+			}
+			{
+				key = "<leader>wq";
+				action = ":wq<CR>";
+				options.desc = "Save and quit";
+			}
+
+		];
   };
 
-  xdg.configFile."nvim".source = ./lua;
 }
